8ebb2fbe880c1ff7b08f1589a252b17bbf953298
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2024-04-11 02:31:59 +0300

    get input as args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    let query = &args[1];
+    let file_name = &args[2];
+
+    println!("{:?}", args);
+    println!("Searching for {}", file_name);
+    println!("In file {}", query);
 }
