4af57a4eeaf2c2209084e294ad064a59f25b952c
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2024-04-11 02:49:59 +0300

    refactor input / config parsing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
+use std::env;
+use std::fs;
+use std::process;
+
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_name = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("{:?}", args);
-    println!("Searching for {}", file_name);
-    println!("In file {}", query);
+    println!("Searching for {}", config.file_name);
+    println!("In file {}", config.query);
 
     let contents =
-        std::fs::read_to_string(file_name).expect("Something went wrong reading the file");
+        fs::read_to_string(config.file_name).expect("Something went wrong reading the file");
 
     println!("With text: \n{}", contents);
+}
+
+struct Config {
+    query: String,
+    file_name: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_name = args[2].clone();
+
+        Ok(Config { query, file_name })
+    }
 }
