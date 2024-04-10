0ab0e704616f2d72a17158750675d6e4152d2a37
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2024-04-11 02:58:51 +0300

    split my code to lib and main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use mini_grep::run;
+use mini_grep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,31 +18,5 @@ fn main() {
     if let Err(e) = run(config) {
         println!("Application error: {}", e);
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_name)?;
-
-    println!("With text: \n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_name: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_name = args[2].clone();
-
-        Ok(Config { query, file_name })
     }
 }
